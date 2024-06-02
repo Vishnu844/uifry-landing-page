cd73f282873ca7dea74d7f8e6a85711862f9a1de
Vishnu844 <vishnu4017@gmail.com>
2024-06-02 19:09:17 +0530

    updated page title and readme file

--- src/components/Star.jsx
+++ src/components/Star.jsx
@@ -1,7 +1,7 @@
 export default function Star({ styles }) {
   return (
     <div className={styles}>
-      <img src="/images/star.png" alt="star" />
+      <img src="/images/Star.png" alt="star" />
     </div>
   );
 }
